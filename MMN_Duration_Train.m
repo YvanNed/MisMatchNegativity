95f30f799be05d10a96af5102bfecb9c887e6c89
YvanNed <57987947+YvanNed@users.noreply.github.com>
2019-12-12 09:46:03 +0100

    Add files via upload

---
+++ MMN_Duration_Train.m
@@ -0,0 +1,16 @@
+function MMN_Duration_Train
+% =========================================================================
+% 27/11/2019 Y. Nedelec
+% =========================================================================
+% basic MMN duration with at least 2 standards (o) between a deviant (x)
+% o o o o o x o o o o o x o o x ...
+% std duration (o) = 500 ms
+% dev duration (x) = 400 475 525 600 ms
+% ISI              = [800-1200] ms
+% =========================================================================
+% The sounds need to be created before with the function [insert name]
+% and then placed in a folder [insert name] at the location [insert path]
+% =========================================================================
+% We consider that we have 10 min of testing either acc or decc or cst
+% speed.
+end
